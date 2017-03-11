08b70be95913481caa5433868e1c99a87a1f6d8b
NeuralNetworkingTechnologies <software.pro.08@gmail.com>
2017-03-10 19:43:41 -0800

    add of core.py (core layer)

---
+++ src/tflobj/core/core.py
@@ -0,0 +1,13 @@
+
+'''
+    The root of the the Core Layer Objects.
+'''
+class CoreLayer(TfLearnObj):
+
+    '''
+        Constructs the Core Layer.
+    '''
+    def __init__(self):
+
+        # Invoke the super class constructor
+        super(CoreLayer, self).__init__()
